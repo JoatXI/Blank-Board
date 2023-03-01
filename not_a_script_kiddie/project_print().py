bb9460202cb24ad47334d07e4a3eab1842a0cf9f
Michael Paterson <michaelpaterson194@gmail.com>
2023-03-01 11:46:16 +0000

    initialised user voice command function

--- not_a_script_kiddie/project_print().py
+++ not_a_script_kiddie/project_print().py
@@ -14,3 +14,17 @@ engine.setProperty("voice", voices[1].id)
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+    
+def taking_voice_commands():
+    try:
+        with sr.Microphone() as source:
+            print("listening...")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if "print" in command: # print() being the name of this AI (Note: should progamme AI with the abililty to change it's name through user command)
+                command = command.replace("print", '')
+                speak(command)
+    except:
+        pass
+    return command
