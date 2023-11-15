4e30f7ed6c9e3bf919639eccd703425fade195ad
JoatXI <michaelpaterson194@gmail.com>
2023-11-15 18:18:57 +0000

    attempting problems

--- data_structure_&_algorithm/binary_search_practice.py
+++ data_structure_&_algorithm/binary_search_practice.py
@@ -32,3 +32,51 @@ def locate_card(cards, query):
     
 
 locate_card([13, 11, 10, 7, 4, 3, 1, 0], 11)
+
+# Here's how binary search can be applied to our problem:
+
+# Find the middle element of the list.
+# If it matches queried number, return the middle position as the answer.
+# If it is less than the query number, then search the first half of the list
+# If it is greater than the queried number, then search the second half of the list
+# If no more elements remain, return -1.
+
+def test_location(cards, query, mid):
+    mid_number = cards[mid]
+    if mid_number == query:
+        print("mid: ", mid, " mid_number: ", mid_number)
+        if mid - 1 >= 0 and cards[mid - 1] == query:
+            return "left"
+        else:
+            return "found"
+    elif mid_number < query:
+        return "left"
+    else:
+        return "right"
+
+
+def locate_card2(cards, query):
+    
+    low = 0
+    high = len(cards) - 1
+    
+    #Finds the middle number of the list
+    while low <= high:
+        print("low: ", low, ", high: ", high)
+        # double // is used in case where (low + high) is not divisible by 2
+        mid = (low + high) // 2 # this can also be done as: math.floor((start + end) / 2).. math needs to be imported for this to work.
+        
+        result = test_location(cards, query, mid)
+        
+        if result == "found":
+            return mid
+        elif result == "left":
+            high = mid - 1
+        elif result == "right":
+            low = mid + 1
+    
+    # if query is not found            
+    return -1
+        
+        
+locate_card2([13, 11, 10, 7, 4, 3, 1, 0], 11)
